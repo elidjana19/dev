bc7fff3432c5266b85dc0fc1fd7f2b782b2519c0
Elidjana <elidjana.nako@yahoo.com>
2024-06-07 13:36:24 +0200

    animation test

--- pokemon/pokemon/pokemon.js
+++ pokemon/pokemon/pokemon.js
@@ -10,7 +10,7 @@ function getData() {
       return response.json();
     })
     .then((data) => {
-      console.log(data.results);
+      //console.log(data.results);
       displayCards(data.results); //data.result is an array of objects
     });
 }
@@ -23,13 +23,14 @@ function displayCards(results) {
 
     const url = pokemon.url;
     const segments = url.split("/").filter(Boolean); // Split by '/' and remove empty elements
-    const pokemonId = segments[segments.length - 1];
-    console.log(pokemonId); //pokemonId holds all the ID-s
+    // console.log(segments) 
+
+    const pokemonId = segments[segments.length-1]; //retrives the last index of the segments[] where is the id
 
     const cardInnerHTML = `
     <div class="img-container">
         <img src="https://pokeres.bastionbot.org/images/pokemon/${
-            pokemon.id
+            pokemonId
         }.png" alt="">
     </div>
     <div class="info">
@@ -40,19 +41,41 @@ function displayCards(results) {
 
     divPokemon.innerHTML = cardInnerHTML;
     divPokemon.classList.add("card");
+  
+
+  
 
     divPokemon.style.backgroundColor = getRandomColor();
 
+  
     pokContainer.appendChild(divPokemon);
+
+
   });
+
+
+//to access the cards
+const cards = document.querySelectorAll(".card");
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    console.log("Card clicked:", card);
+  });
+});
+
+
+
+
 }
 
  function getRandomColor() {
   var hue = Math.floor(Math.random() * 360); //hue= type of color
-  
 
-  var saturation = Math.floor(Math.random() * 20) + 80;  //intensity or vividness of color
+  var saturation = Math.floor(Math.random() * 20) + 80;  
   var lightness = Math.floor(Math.random() * 20) + 80; 
   
   return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
 }
+
+
+
+     
